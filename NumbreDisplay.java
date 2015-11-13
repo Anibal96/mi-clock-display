4ad80e09a5fe119cf1309f35b9e49600e71b4c20
Anibal <anibaltascon@gmail.com>
2015-11-13 12:11:46 +0100

    Añadido metodo increment

--- NumbreDisplay.java
+++ NumbreDisplay.java
@@ -10,34 +10,49 @@ public class NumbreDisplay
     private int limitNumber;
     
     private int display;
-    
-    private int display1;
 
     /**
      * Constructor for objects of class NumbreDisplay
      */
-    public NumbreDisplay(int limitNumber,int display2)
+    public NumbreDisplay(int limitNumber1)
     {
         display = 0;
-        display1 = display2;
+        limitNumber = limitNumber1;
     }
 
     /**
      * Método setter que fija el valor actual del display al valor pasado como parámetro.
      */
-    public void setValue()
+    public void setValue(int display1)
     {
         display = display1;
     }
+    /**
+     * Muestra display con 2 elementos.
+     */
     public String getDisplayValue()
     {
        String displaYc = "%02d";
        String resultado = String.format(displaYc,display);
        return resultado;
     }
-    
+    /**
+     * Devuelve display.
+     */
     public int getValue()
     {
         return display;
+    }
+    /**
+     * Suma 1 a display.
+     */
+    public void increment()
+    {
+        if (display == limitNumber){
+            display = 0;
+        }
+        else{
+            display = display + 1;
+        }
     }
 }
